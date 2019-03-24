1392140ce51f41e7bc31d614dbed7dde2bd63a44
Erfan Abdi <erfangplus@gmail.com>
2019-03-24 18:12:00 +0200

    runtime elfpatch Q libs
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- makeQ.sh
+++ makeQ.sh
@@ -39,8 +39,59 @@ cp -fpr $thispath/vndk-sp-28-arm32-replace/libcutils.so $1/lib/vndk-sp-28/
 cp -fpr $thispath/vndk-sp-28-arm64-replace/libcutils.so $1/lib64/vndk-sp-28/
 
 # Q patched libs
-cp -fpr $thispath/vndk-28-arm32-Qpatch/* $1/lib/vndk-28/
-cp -fpr $thispath/vndk-sp-28-arm32-Qpatch/* $1/lib/vndk-sp-28/
+toolsdir="$thispath/tools"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    patchelf="$toolsdir/linux/bin/patchelf"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    patchelf="$toolsdir/mac/bin/patchelf"
+else
+    echo "Not Supported OS for patchelf"
+fi
 
-cp -fpr $thispath/vndk-27-arm32-Qpatch/* $1/lib/vndk-27/
-cp -fpr $thispath/vndk-27-arm64-Qpatch/* $1/lib64/vndk-27/
+$patchelf --add-needed libqinder.so $1/lib/vndk-28/libbinder.so
+$patchelf --add-needed libqi.so $1/lib/vndk-28/libui.so
+$patchelf --add-needed libqinder.so $1/lib/vndk-28/libui.so
+$patchelf --add-needed libqui.so $1/lib/vndk-28/libgui.so
+$patchelf --add-needed libqedia_helper.so $1/lib/vndk-28/libmedia_helper.so
+$patchelf --add-needed libqedia_omx.so $1/lib/vndk-28/libmedia_omx.so
+
+cp -fpr $1/lib/libbinder.so $1/lib/vndk-28/libqinder.so
+cp -fpr $1/lib/libui.so $1/lib/vndk-28/libqi.so
+cp -fpr $1/lib/libgui.so $1/lib/vndk-28/libqui.so
+cp -fpr $1/lib/libmedia_helper.so $1/lib/vndk-28/libqedia_helper.so
+cp -fpr $1/lib/libmedia_omx.so $1/lib/vndk-28/libqedia_omx.so
+
+$patchelf --add-needed libqase.so $1/lib/vndk-sp-28/libbase.so
+$patchelf --add-needed libqwbinder.so $1/lib/vndk-sp-28/libhidltransport.so
+$patchelf --add-needed libqidltransport.so $1/lib/vndk-sp-28/libhidltransport.so
+
+cp -fpr $1/lib/libbase.so $1/lib/vndk-sp-28/libqase.so
+cp -fpr $1/lib/libhwbinder.so $1/lib/vndk-sp-28/libqwbinder.so
+cp -fpr $1/lib/libhidltransport.so $1/lib/vndk-sp-28/libqidltransport.so
+cp -fpr $1/lib/libbinderthreadstate.so $1/lib/vndk-sp-28/libbinderthreadstate.so
+
+$patchelf --add-needed libol.so $1/lib/vndk-27/libgraphicsenv.so
+
+$patchelf --add-needed libqinder.so $1/lib64/vndk-28/libbinder.so
+$patchelf --add-needed libqi.so $1/lib64/vndk-28/libui.so
+$patchelf --add-needed libqinder.so $1/lib64/vndk-28/libui.so
+$patchelf --add-needed libqui.so $1/lib64/vndk-28/libgui.so
+$patchelf --add-needed libqedia_helper.so $1/lib64/vndk-28/libmedia_helper.so
+$patchelf --add-needed libqedia_omx.so $1/lib64/vndk-28/libmedia_omx.so
+
+cp -fpr $1/lib64/libbinder.so $1/lib64/vndk-28/libqinder.so
+cp -fpr $1/lib64/libui.so $1/lib64/vndk-28/libqi.so
+cp -fpr $1/lib64/libgui.so $1/lib64/vndk-28/libqui.so
+cp -fpr $1/lib64/libmedia_helper.so $1/lib64/vndk-28/libqedia_helper.so
+cp -fpr $1/lib64/libmedia_omx.so $1/lib64/vndk-28/libqedia_omx.so
+
+$patchelf --add-needed libqase.so $1/lib64/vndk-sp-28/libbase.so
+$patchelf --add-needed libqwbinder.so $1/lib64/vndk-sp-28/libhidltransport.so
+$patchelf --add-needed libqidltransport.so $1/lib64/vndk-sp-28/libhidltransport.so
+
+cp -fpr $1/lib64/libbase.so $1/lib64/vndk-sp-28/libqase.so
+cp -fpr $1/lib64/libhwbinder.so $1/lib64/vndk-sp-28/libqwbinder.so
+cp -fpr $1/lib64/libhidltransport.so $1/lib64/vndk-sp-28/libqidltransport.so
+cp -fpr $1/lib64/libbinderthreadstate.so $1/lib64/vndk-sp-28/libbinderthreadstate.so
+
+$patchelf --add-needed libol.so $1/lib64/vndk-27/libgraphicsenv.so
