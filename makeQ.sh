4d70217d9f1e5c59757433c0d185bb7c7f2c1c4a
Erfan Abdi <erfangplus@gmail.com>
2019-04-03 23:22:30 +0300

    fix _ZN7android8hardware7details15wouldLogVerboseEv
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- makeQ.sh
+++ makeQ.sh
@@ -54,12 +54,14 @@ $patchelf --add-needed libqinder.so $1/lib/vndk-28/libui.so
 $patchelf --add-needed libqui.so $1/lib/vndk-28/libgui.so
 $patchelf --add-needed libqedia_helper.so $1/lib/vndk-28/libmedia_helper.so
 $patchelf --add-needed libqedia_omx.so $1/lib/vndk-28/libmedia_omx.so
+$patchelf --add-needed qndroid.hardware.configstore-utils.so $1/lib/vndk-28/android.hardware.configstore-utils.so
 
 cp -fpr $1/lib/libbinder.so $1/lib/vndk-28/libqinder.so
 cp -fpr $1/lib/libui.so $1/lib/vndk-28/libqi.so
 cp -fpr $1/lib/libgui.so $1/lib/vndk-28/libqui.so
 cp -fpr $1/lib/libmedia_helper.so $1/lib/vndk-28/libqedia_helper.so
 cp -fpr $1/lib/libmedia_omx.so $1/lib/vndk-28/libqedia_omx.so
+cp -fpr $1/lib/android.hardware.configstore-utils.so $1/lib/vndk-28/qndroid.hardware.configstore-utils.so
 
 $patchelf --add-needed libqase.so $1/lib/vndk-sp-28/libbase.so
 $patchelf --add-needed libqwbinder.so $1/lib/vndk-sp-28/libhidltransport.so
@@ -78,12 +80,14 @@ $patchelf --add-needed libqinder.so $1/lib64/vndk-28/libui.so
 $patchelf --add-needed libqui.so $1/lib64/vndk-28/libgui.so
 $patchelf --add-needed libqedia_helper.so $1/lib64/vndk-28/libmedia_helper.so
 $patchelf --add-needed libqedia_omx.so $1/lib64/vndk-28/libmedia_omx.so
+$patchelf --add-needed qndroid.hardware.configstore-utils.so $1/lib64/vndk-28/android.hardware.configstore-utils.so
 
 cp -fpr $1/lib64/libbinder.so $1/lib64/vndk-28/libqinder.so
 cp -fpr $1/lib64/libui.so $1/lib64/vndk-28/libqi.so
 cp -fpr $1/lib64/libgui.so $1/lib64/vndk-28/libqui.so
 cp -fpr $1/lib64/libmedia_helper.so $1/lib64/vndk-28/libqedia_helper.so
 cp -fpr $1/lib64/libmedia_omx.so $1/lib64/vndk-28/libqedia_omx.so
+cp -fpr $1/lib64/android.hardware.configstore-utils.so $1/lib64/vndk-28/qndroid.hardware.configstore-utils.so
 
 $patchelf --add-needed libqase.so $1/lib64/vndk-sp-28/libbase.so
 $patchelf --add-needed libqwbinder.so $1/lib64/vndk-sp-28/libhidltransport.so
