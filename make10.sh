50758cb28062880204630a61d34a99a9fe36bc7c
Erfan Abdi <erfangplus@gmail.com>
2019-05-09 00:35:07 +0300

    Q dp3 updates
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- make10.sh
+++ make10.sh
@@ -26,10 +26,7 @@ cp -fprn $thispath/vndk-sp-28-arm64/* $1/lib64/vndk-sp-28/
 cp -fpr $thispath/vndk-28-arm32-replace/libbinder.so $1/lib/vndk-28/
 cp -fpr $thispath/vndk-28-arm64-replace/libbinder.so $1/lib64/vndk-28/
 # Q lib
-cp -fpr $thispath/vndk-28-arm32-replace/libselinux.so $1/lib/vndk-28/
-cp -fpr $thispath/vndk-28-arm64-replace/libselinux.so $1/lib64/vndk-28/
-cp -fpr $thispath/vndk-28-arm32-replace/libstagefright_foundation.so $1/lib/vndk-28/
-cp -fpr $thispath/vndk-28-arm32-replace/libcamera_client.so $1/lib/vndk-28/
+cp -fpr $1/lib/libstagefright_foundation.so $1/lib/vndk-28/
 # modded lib
 cp -fpr $thispath/vndk-28-arm32-replace/libprocinfo.so $1/lib/vndk-28/
 cp -fpr $thispath/vndk-28-arm32-replace/libziparchive.so $1/lib/vndk-28/
@@ -55,6 +52,7 @@ $patchelf --add-needed libqui.so $1/lib/vndk-28/libgui.so
 $patchelf --add-needed libqedia_helper.so $1/lib/vndk-28/libmedia_helper.so
 $patchelf --add-needed libqedia_omx.so $1/lib/vndk-28/libmedia_omx.so
 $patchelf --add-needed qndroid.hardware.configstore-utils.so $1/lib/vndk-28/android.hardware.configstore-utils.so
+$patchelf --add-needed libqelinux.so $1/lib/vndk-28/libselinux.so
 
 cp -fpr $1/lib/libbinder.so $1/lib/vndk-28/libqinder.so
 cp -fpr $1/lib/libui.so $1/lib/vndk-28/libqi.so
@@ -62,15 +60,18 @@ cp -fpr $1/lib/libgui.so $1/lib/vndk-28/libqui.so
 cp -fpr $1/lib/libmedia_helper.so $1/lib/vndk-28/libqedia_helper.so
 cp -fpr $1/lib/libmedia_omx.so $1/lib/vndk-28/libqedia_omx.so
 cp -fpr $1/lib/android.hardware.configstore-utils.so $1/lib/vndk-28/qndroid.hardware.configstore-utils.so
+cp -fpr $1/lib/libselinux.so $1/lib/vndk-28/libqelinux.so
 
 $patchelf --add-needed libqase.so $1/lib/vndk-sp-28/libbase.so
 $patchelf --add-needed libqwbinder.so $1/lib/vndk-sp-28/libhidltransport.so
 $patchelf --add-needed libqidltransport.so $1/lib/vndk-sp-28/libhidltransport.so
+$patchelf --add-needed libqutils.so $1/lib/vndk-sp-28/libcutils.so
 
 cp -fpr $1/lib/libbase.so $1/lib/vndk-sp-28/libqase.so
 cp -fpr $1/lib/libhwbinder.so $1/lib/vndk-sp-28/libqwbinder.so
 cp -fpr $1/lib/libhidltransport.so $1/lib/vndk-sp-28/libqidltransport.so
 cp -fpr $1/lib/libbinderthreadstate.so $1/lib/vndk-sp-28/libbinderthreadstate.so
+cp -fpr $1/lib/libcutils.so $1/lib/vndk-sp-28/libqutils.so
 
 $patchelf --add-needed libol.so $1/lib/vndk-27/libgraphicsenv.so
 
@@ -81,6 +82,7 @@ $patchelf --add-needed libqui.so $1/lib64/vndk-28/libgui.so
 $patchelf --add-needed libqedia_helper.so $1/lib64/vndk-28/libmedia_helper.so
 $patchelf --add-needed libqedia_omx.so $1/lib64/vndk-28/libmedia_omx.so
 $patchelf --add-needed qndroid.hardware.configstore-utils.so $1/lib64/vndk-28/android.hardware.configstore-utils.so
+$patchelf --add-needed libqelinux.so $1/lib64/vndk-28/libselinux.so
 
 cp -fpr $1/lib64/libbinder.so $1/lib64/vndk-28/libqinder.so
 cp -fpr $1/lib64/libui.so $1/lib64/vndk-28/libqi.so
@@ -88,14 +90,17 @@ cp -fpr $1/lib64/libgui.so $1/lib64/vndk-28/libqui.so
 cp -fpr $1/lib64/libmedia_helper.so $1/lib64/vndk-28/libqedia_helper.so
 cp -fpr $1/lib64/libmedia_omx.so $1/lib64/vndk-28/libqedia_omx.so
 cp -fpr $1/lib64/android.hardware.configstore-utils.so $1/lib64/vndk-28/qndroid.hardware.configstore-utils.so
+cp -fpr $1/lib64/libselinux.so $1/lib64/vndk-28/libqelinux.so
 
 $patchelf --add-needed libqase.so $1/lib64/vndk-sp-28/libbase.so
 $patchelf --add-needed libqwbinder.so $1/lib64/vndk-sp-28/libhidltransport.so
 $patchelf --add-needed libqidltransport.so $1/lib64/vndk-sp-28/libhidltransport.so
+$patchelf --add-needed libqutils.so $1/lib64/vndk-sp-28/libcutils.so
 
 cp -fpr $1/lib64/libbase.so $1/lib64/vndk-sp-28/libqase.so
 cp -fpr $1/lib64/libhwbinder.so $1/lib64/vndk-sp-28/libqwbinder.so
 cp -fpr $1/lib64/libhidltransport.so $1/lib64/vndk-sp-28/libqidltransport.so
 cp -fpr $1/lib64/libbinderthreadstate.so $1/lib64/vndk-sp-28/libbinderthreadstate.so
+cp -fpr $1/lib64/libcutils.so $1/lib64/vndk-sp-28/libqutils.so
 
 $patchelf --add-needed libol.so $1/lib64/vndk-27/libgraphicsenv.so
