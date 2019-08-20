e365025e07e42f67c84b10eb801b544ff36a2465
Erfan Abdi <erfangplus@gmail.com>
2019-08-21 01:55:50 +0430

    9: force replace all vndk28
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- make9.sh
+++ make9.sh
@@ -18,8 +18,8 @@ cp -fprn $thispath/vndk-sp-27-arm32/* $1/lib/vndk-sp-27/
 cp -fprn $thispath/vndk-sp-27-arm64/* $1/lib64/vndk-sp-27/
 mkdir -p $1/lib/vndk-28
 mkdir -p $1/lib64/vndk-28
-cp -fprn $thispath/vndk-28-arm32/* $1/lib/vndk-28/
-cp -fprn $thispath/vndk-28-arm64/* $1/lib64/vndk-28/
+cp -fpr $thispath/vndk-28-arm32/* $1/lib/vndk-28/
+cp -fpr $thispath/vndk-28-arm64/* $1/lib64/vndk-28/
 # exclude_srcs libs
 cp -fpr $thispath/vndk-28-arm32-replace/libgui.so $1/lib/vndk-28/
 cp -fpr $thispath/vndk-28-arm64-replace/libgui.so $1/lib64/vndk-28/
@@ -27,8 +27,8 @@ cp -fpr $thispath/vndk-28-arm32-replace/libbinder.so $1/lib/vndk-28/
 cp -fpr $thispath/vndk-28-arm64-replace/libbinder.so $1/lib64/vndk-28/
 mkdir -p $1/lib/vndk-sp-28
 mkdir -p $1/lib64/vndk-sp-28
-cp -fprn $thispath/vndk-sp-28-arm32/* $1/lib/vndk-sp-28/
-cp -fprn $thispath/vndk-sp-28-arm64/* $1/lib64/vndk-sp-28/
+cp -fpr $thispath/vndk-sp-28-arm32/* $1/lib/vndk-sp-28/
+cp -fpr $thispath/vndk-sp-28-arm64/* $1/lib64/vndk-sp-28/
 # exclude_srcs libs
 cp -fpr $thispath/vndk-sp-28-arm32-replace/libcutils.so $1/lib/vndk-sp-28/
 cp -fpr $thispath/vndk-sp-28-arm64-replace/libcutils.so $1/lib64/vndk-sp-28/
