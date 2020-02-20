d1b07095247eca81290b5ebf88b4aed0ecc5009b
Erfan Abdi <erfangplus@gmail.com>
2020-02-20 16:40:33 +0330

    Setup R vndk

---
+++ makeR.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+# Make VNDK symlinks
+rm -rf $systempath/lib/vndk-29 $systempath/lib/vndk-sp-29
+rm -rf $systempath/lib/vndk-28 $systempath/lib/vndk-sp-28
+rm -rf $systempath/lib64/vndk-29 $systempath/lib64/vndk-sp-29
+rm -rf $systempath/lib64/vndk-28 $systempath/lib64/vndk-sp-28
+
+ln -s  /apex/com.android.vndk.v29/lib $systempath/lib/vndk-29
+ln -s  /apex/com.android.vndk.v28/lib $systempath/lib/vndk-28
+ln -s  /apex/com.android.vndk.v29/lib $systempath/lib/vndk-sp-29
+ln -s  /apex/com.android.vndk.v28/lib $systempath/lib/vndk-sp-28
+
+ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-29
+ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-28
+ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-sp-29
+ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-sp-28
+
+# Extract VNDK apex to system
+7z x -y $thispath/R/com.android.vndk.v28.apex.7z -o$systempath/system_ext/apex/
+7z x -y $thispath/R/com.android.vndk.v29.apex.7z -o$systempath/system_ext/apex/
