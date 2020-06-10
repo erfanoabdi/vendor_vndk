26d33260548d064de17c459b79fba91be1c63236
Erfan Abdi <erfangplus@gmail.com>
2020-06-10 22:38:00 +0430

    R -> 11

--- make11.sh
+++ make11.sh
@@ -20,5 +20,5 @@ ln -s  /apex/com.android.vndk.v29/lib64 $systempath/lib64/vndk-sp-29
 ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-sp-28
 
 # Extract VNDK apex to system
-7z x -y $thispath/R/com.android.vndk.v28.apex.7z -o$systempath/system_ext/apex/ 2>/dev/null >> "$TMPDIR"/zip.log
-7z x -y $thispath/R/com.android.vndk.v29.apex.7z -o$systempath/system_ext/apex/ 2>/dev/null >> "$TMPDIR"/zip.log
+7z x -y $thispath/11/com.android.vndk.v28.apex.7z -o$systempath/system_ext/apex/ 2>/dev/null >> "$TMPDIR"/zip.log
+7z x -y $thispath/11/com.android.vndk.v29.apex.7z -o$systempath/system_ext/apex/ 2>/dev/null >> "$TMPDIR"/zip.log
